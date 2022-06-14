d8e690ddc7c51b929c1d35ce9a26d3bc52e95e0d
helloWooYoung <hello620998@gmail.com>
2022-06-14 21:05:19 +0900

    wooyoung1.lee ServiceImpl Exception 처리 추가

--- src/main/java/com/example/memberex/services/member/MemberSignUpServiceImpl.java
+++ src/main/java/com/example/memberex/services/member/MemberSignUpServiceImpl.java
@@ -7,7 +7,10 @@ import org.springframework.stereotype.Service;
 @Service
 public class MemberSignUpServiceImpl implements MemberSignUpService {
     @Override
-    public MemberDto postmemberSignUp(MemberDto dto) {
+    public MemberDto postmemberSignUp(MemberDto dto) throws Exception{
+
+        if(dto.equals(null)) throw new NullPointerException("Data is null");
+
         return null;
     }
 
